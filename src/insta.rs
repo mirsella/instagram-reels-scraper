8d76839d3c871a4a5bf61116dfa455b89cfb3c1a
mirsella <mirsella@protonmail.com>
2023-12-30 16:42:51 +0100

    gamp

--- src/insta.rs
+++ src/insta.rs
@@ -48,14 +48,15 @@ fn login(config: &Config) -> Result<()> {
         {
             sleep(Duration::from_millis(100));
         }
-        thread::sleep(Duration::from_secs(2));
         tab.wait_until_navigated()?;
+        thread::sleep(Duration::from_secs(2));
         trace!("finish waiting for redirect");
         if let Ok(el) = tab.find_element("button[type=button]") {
             trace!("save info");
             el.click()?;
+            tab.wait_until_navigated()?;
+            thread::sleep(Duration::from_secs(2));
         }
-        thread::sleep(Duration::from_secs(2));
         tab.wait_until_navigated()?;
         thread::sleep(Duration::from_secs(2));
     }
